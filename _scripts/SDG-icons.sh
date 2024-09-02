868da3cd40f7eb6cdcd438ef828b5593102725d9
Jose Rafael Ferrer-Paris <jr.ferrer.paris@gmail.com>
2024-09-02 16:32:03 +1000

    What are my contributions to the  SDGs?

--- _scripts/SDG-icons.sh
+++ _scripts/SDG-icons.sh
@@ -1,8 +1,14 @@
 mkdir -p sandbox
 mkdir -p images
-wget --continue https://globalgoalscms.co.uk/wp-content/uploads/2021/10/goal-15.zip --output-document=sandbox/goal-15.zip
-unzip -u sandbox/goal-15.zip -D sandbox
+
+for g in 2 6 11 15 14
+do 
+    wget --continue https://globalgoalscms.co.uk/wp-content/uploads/2021/10/goal-${g}.zip --output-document=sandbox/goal-${g}.zip
+    unzip -u sandbox/goal-${g}.zip -d sandbox
+    mv -n sandbox/goal-${g}/GOAL_${g}_TARGETS/GOAL_${g}_*SVG/GOAL_${g}_TARGET_${g}.* images
+    mv sandbox/goal-${g}/GOAL_${g}_PRIMARY_ICON/GOAL_${g}_SVG/TheGlobalGoals_Icons_*_Goal_${g}.svg images 
+done
 
 
-mv sandbox/goal-15/GOAL_15_PRIMARY_ICON/GOAL_15_SVG/TheGlobalGoals_Icons_Color_Goal_15.svg images 
-mv sandbox/goal-15/GOAL_15_TARGETS/GOAL_15_TARGETS_SVG/GOAL_15_TARGET_15.* images 
+ 
+mv sandbox/goal-15/GOAL_15_TARGETS/GOAL_15_TARGETS_SVG/GOAL_15_TARGET_11.* images 
